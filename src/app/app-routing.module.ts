3a1831cfbd219195e925d925df77091a135001cb
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-06-19 11:55:50 +0530

    done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,137 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
+import { StudentComponent } from './student/student.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { SubjectComponent } from './subject/subject.component';
+import { CollegeComponent } from './college/college.component';
+import { CourseComponent } from './course/course.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { RoleListComponent } from './role/role-list.component';
+import { UserListComponent } from './user/user-list.component';
+import { CollegeListComponent } from './college/college-list.component';
+import { CourseListComponent } from './course/course-list.component';
+import { FacultyListComponent } from './faculty/faculty-list.component';
+import { MarksheetListComponent } from './marksheet/marksheet-list.component';
+import { StudentListComponent } from './student/student-list.component';
+import { TimetableListComponent } from './timetable/timetable-list.component';
+import { SubjectListComponent } from './subject/subject-list.component';
+
+const routes: Routes = [
+  {
+    path: 'role',
+    component: RoleComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: 'faculty',
+    component: FacultyComponent
+  },
+  {
+    path: 'subject',
+    component: SubjectComponent
+  },
+  {
+    path: 'college',
+    component: CollegeComponent
+  },
+  {
+    path: 'course',
+    component: CourseComponent
+  },
+  {
+    path: 'marksheet',
+    component: MarksheetComponent
+  },
+  {
+    path: 'timetable',
+    component: TimetableComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'role-list',
+    component: RoleListComponent
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent
+  },
+  {
+    path: 'college-list',
+    component: CollegeListComponent
+  },
+  {
+    path: 'course-list',
+    component: CourseListComponent
+  },
+  {
+    path: 'faculty-list',
+    component: FacultyListComponent
+  },
+  {
+    path: 'marksheet-list',
+    component: MarksheetListComponent
+  },
+  {
+    path: 'student-list',
+    component: StudentListComponent
+  },
+  {
+    path: 'subject-list',
+    component: SubjectListComponent
+  },
+  {
+    path: 'timetable-list',
+    component: TimetableListComponent
+  },
+  {
+    path: 'user/:id',
+    component: UserComponent
+  },
+  {
+    path: 'role/:id',
+    component: RoleComponent
+  },
+  {
+    path: 'college/:id',
+    component: CollegeComponent
+  },
+  {
+    path: 'course/:id',
+    component: CourseComponent
+  },
+  {
+    path: 'faculty/:id',
+    component: FacultyComponent
+  },
+  {
+    path: 'marksheet/:id',
+    component: MarksheetComponent
+  },
+  {
+    path: 'student/:id',
+    component: StudentComponent
+  },
+  {
+    path: 'subject/:id',
+    component: SubjectComponent
+  },
+  {
+    path: 'timetable/:id',
+    component: TimetableComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RoleComponent } from './role/role.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpServiceService } from './http-service.service';
+import { EndpointServiceService } from './endpoint-service.service';
+import { ServiceLocatorService } from './service-locator.service';
+import { FormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
+import { StudentComponent } from './student/student.component';
+import { CollegeComponent } from './college/college.component';
+import { CourseComponent } from './course/course.component';
+import { TimetableComponent } from './timetable/timetable.component';
+import { SubjectComponent } from './subject/subject.component';
+import { MarksheetComponent } from './marksheet/marksheet.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RoleListComponent } from './role/role-list.component';
+import { UserListComponent } from './user/user-list.component';
+import { CollegeListComponent } from './college/college-list.component';
+import { CourseListComponent } from './course/course-list.component';
+import { FacultyListComponent } from './faculty/faculty-list.component';
+import { MarksheetListComponent } from './marksheet/marksheet-list.component';
+import { StudentListComponent } from './student/student-list.component';
+import { TimetableListComponent } from './timetable/timetable-list.component';
+import { SubjectListComponent } from './subject/subject-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoleComponent,
+    UserComponent,
+    StudentComponent,
+    CollegeComponent,
+    CourseComponent,
+    TimetableComponent,
+    SubjectComponent,
+    MarksheetComponent,
+    FacultyComponent,
+    FooterComponent,
+    NavbarComponent,
+    RoleListComponent,
+    UserListComponent,
+    CollegeListComponent,
+    CourseListComponent,
+    FacultyListComponent,
+    MarksheetListComponent,
+    StudentListComponent,
+    TimetableListComponent,
+    SubjectListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    HttpServiceService,
+    EndpointServiceService,
+    ServiceLocatorService,
+    AuthenticatorResponse
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
